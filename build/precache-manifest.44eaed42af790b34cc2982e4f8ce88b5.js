47b5f509b8186582e9bbe09c505f3dd2604a67de
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-02 21:20:03 +0100

    add build dir and remove from gitignore

---
+++ build/precache-manifest.44eaed42af790b34cc2982e4f8ce88b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91d1fc88ebf32617737cf0b7463efef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e29078d8b21173a9f21e",
+    "url": "/static/css/main.658eaac9.chunk.css"
+  },
+  {
+    "revision": "57eb96d26530c212567f",
+    "url": "/static/js/2.b5499d80.chunk.js"
+  },
+  {
+    "revision": "e29078d8b21173a9f21e",
+    "url": "/static/js/main.09322954.chunk.js"
+  },
+  {
+    "revision": "ec396816c40c610230cc",
+    "url": "/static/js/runtime-main.a417407a.js"
+  }
+]);
